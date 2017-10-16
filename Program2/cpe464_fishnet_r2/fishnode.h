1fb486c750a369129892bf22a6a8ccb30cef3128
Zachary <zeagan2@gmail.com>
2017-10-16 15:08:17 -0700

    Transfer from laptop (wifi died)

--- Program2/cpe464_fishnet_r2/fishnode.h
+++ Program2/cpe464_fishnet_r2/fishnode.h
@@ -1,11 +1,14 @@
 #ifndef FISHNODE_DEF
 #define FISHNODE_DEF
 
-int fishnode_l2_receive(void *l2frame);
+#define ARP_TIMEOUT 180
 
+
+int fishnode_l2_receive(void *l2frame);
 int fish_l2_send(void *l3frame, fnaddr_t next_hop, int len);
-void arp_received(void *l2frame);
 void send_arp_request(fnaddr_t l3addr);
+
+void arp_received(void *l2frame);
 void add_arp_entry(fn_l2addr_t l2addr, fnaddr_t addr, int timeout);
 //void resolve_fnaddr(fnaddr_t addr, arp_resolution_cb cb, void *param);
 
@@ -16,5 +19,22 @@ typedef struct l2Header
    uint16_t chk;
    uint16_t len;
 } __attribute__((packed)) l2Header;
+
+typedef struct l3Header
+{
+   uint8_t ttl;
+   uint8_t prot;
+   uint8_t pid[4];
+   uint8_t src[4];
+   uint8_t dst[4];
+} __attribute__((packed)) l3Header;
+
+typedef struct arp_header
+{
+   uint32_t type;
+   fnaddr_t l3;
+   fn_l2addr_t l2;
+} __attribute__((packed)) arp_header;
+
 
 #endif
